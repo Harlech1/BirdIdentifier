02f2bf3bfb06c2124e3cf1c734866348a0b8330c
Harlech1 <turkerkizilcik@gmail.com>
2024-12-30 16:39:30 +0300

    initial commit

---
+++ BirdIdentifier/BirdIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdIdentifierApp.swift
+//  BirdIdentifier
+//
+//  Created by Türker Kızılcık on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct BirdIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
